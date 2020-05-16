f511bf65029f61bd8df68451e6595fd87927bb2b
Abd Allah Zidan <eng.abdallahzidan@gmail.com>
2020-05-16 05:20:57 +0200

    Memento design pattern

---
+++ Memento/advanced/NotepadSimpleMementoAdvanced.java
@@ -0,0 +1,118 @@
+package advanced;
+
+import java.util.ArrayList;
+
+class Notepad {
+    private String text;
+
+    public Notepad(String text) {
+        this.text = text;
+
+    }
+
+    public Notepad() {
+        this("initial text");
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public Memento createMemento() {
+        return new Memento(this.text);
+    }
+
+    public void restore(Memento memento) {
+        this.text = memento.getText();
+    }
+
+    class Memento {
+        final private String text; // this can be a custom snapshot object provided by the notepad
+                                   // containing the attributes that need to be saved as a state
+
+        private Memento(String text) {
+            this.text = text;
+        }
+
+        private String getText() {
+            return this.text;
+        }
+    }
+
+}
+
+class CareTaker {
+    private ArrayList<Notepad.Memento> mementos = new ArrayList<>();
+    private Notepad notepad;
+    private int currMemento = -1;
+
+    public CareTaker(Notepad notepad) {
+        this.notepad = notepad;
+    }
+
+    public void addMemento() {
+        this.mementos.add(this.notepad.createMemento());
+        this.currMemento = mementos.size() - 1;
+    }
+
+    public Notepad.Memento getMemento(int index) {
+        this.currMemento = index;
+        return this.mementos.get(index);
+    }
+
+    // this logic could be added in aseperate class that handles doing and undoing
+    // commands
+    public void undo() {
+        if (mementos.size() > 0) {
+            if (this.currMemento > 0)
+                this.currMemento--;
+            else
+                this.currMemento = 0;
+            this.notepad.restore(getMemento(this.currMemento));
+        }
+
+    }
+
+    public void redo() {
+        if (mementos.size() > 0) {
+            if (this.currMemento < mementos.size() - 1)
+                this.currMemento++;
+            else
+                this.currMemento = mementos.size() - 1;
+            this.notepad.restore(getMemento(this.currMemento));
+        }
+
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Notepad np = new Notepad();
+        CareTaker careTaker = new CareTaker(np);
+        np.setText("hello");
+        careTaker.addMemento();
+        np.setText("again");
+        careTaker.addMemento();
+
+        // print current text in the notepad
+        System.out.println(np.getText());
+
+        // undo the last change
+        careTaker.undo();
+        // restore the previous text
+        System.out.println(np.getText());
+
+        // get the next change again
+        careTaker.redo();
+        System.out.println(np.getText());
+
+        // restore specific change point
+        careTaker.undo();
+        System.out.println(np.getText());
+
+    }
+}
---
+++ Memento/simple/NotepadSimpleMemento.java
@@ -0,0 +1,109 @@
+package simple;
+import java.util.ArrayList;
+
+class Notepad {
+    private String text;
+
+    public Notepad(String text) {
+        this.text = text;
+
+    }
+
+    public Notepad() {
+        this("initial text");
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public Memento createMemento() {
+        return new Memento(this.text);
+    }
+
+    public void restore(Memento memento) {
+        this.text = memento.getText();
+    }
+
+}
+
+class Memento {
+    final private String text;
+
+    public Memento(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+}
+
+class CareTaker {
+    private ArrayList<Memento> mementos = new ArrayList<>();
+    private int currMemento = -1;
+
+    public void addMemento(Memento memento) {
+        this.mementos.add(memento);
+        this.currMemento = mementos.size() - 1;
+    }
+
+    public Memento getMemento(int index) {
+        this.currMemento = index;
+        return this.mementos.get(index);
+    }
+
+    public Memento undo() {
+        if (mementos.size() > 0) {
+            if (this.currMemento > 0)
+                this.currMemento--;
+            else
+                this.currMemento = 0;
+            return getMemento(this.currMemento);
+        }
+        return null;
+    }
+
+    public Memento redo() {
+        if (mementos.size() > 0) {
+            if (this.currMemento < mementos.size() - 1)
+                this.currMemento++;
+            else
+                this.currMemento = mementos.size() - 1;
+            return getMemento(this.currMemento);
+        }
+        return null;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Notepad np = new Notepad();
+        CareTaker careTaker = new CareTaker();
+        np.setText("hello");
+        careTaker.addMemento(np.createMemento());
+        np.setText("again");
+        careTaker.addMemento(np.createMemento());
+
+        // print current text in the notepad
+        System.out.println(np.getText());
+
+        // undo the last change
+        np.restore(careTaker.undo());
+        // restore the previous text
+        System.out.println(np.getText());
+
+        // get the next change again
+        np.restore(careTaker.redo());
+        System.out.println(np.getText());
+
+        // restore specific change point
+        np.restore(careTaker.undo());
+        System.out.println(np.getText());
+
+    }
+}
